abb67d31dcf18bb79c82eb18d38e12549a75c310
mcmorki <51241071+mcmorki@users.noreply.github.com>
2019-07-01 16:37:44 -0400

    7/1/2019 4:37 pm

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ var check3 = $("input[name='js-libs']")
 var check4 = $("input[name='node']")
 var check5 = $("input[name='build-tools']")
 var check6 = $("input[name='npm']")
-var check7 = $("input[name='all']")
+var check0 = $("input[name='all']")
 let $total = 0;
 
 // checkStatus function watches the state of the activities selected 
@@ -47,70 +47,54 @@ function checkStatus() {
 
   if (check1.is(':checked')) {
     $total += 100
-    check2.prop('disabled', true);              //If js-frameworks is selected disable the express option
+    check2.prop('disabled', true);              //If js-frameworks is selected add $100 to total and disable the express option
   } else {                                      // else do not disable express if js-frameworks wasnt selected
     check2.prop('disabled', false);
   }
   if (check2.is(':checked')) {
     $total += 100
-    check1.prop('disabled', true)               //If express is selected disable the js-frameworks option
+    check1.prop('disabled', true)               //If express is selected add $100 to total  disable the js-frameworks option
   } else {                                      // else do not disable js-frameworks if express wasnt selected
     check1.prop('disabled', false);
   }
 
   if (check3.is(':checked')) {
     $total += 100
-    check4.prop('disabled', true);              //If js-libs is selected disable the node option
+    check4.prop('disabled', true);              //If js-libs is selected add $100 to total  disable the node option
   } else {                                      // else do not disable node if js-libs wasnt selected
     check4.prop('disabled', false);
   }
   if (check4.is(':checked')) {
     $total += 100
-    check3.prop('disabled', true)                //If node is selected disable the js-libs option
+    check3.prop('disabled', true)                //If node is selected add $100 to total disable the js-libs option
   } else {                                       // else do not disable  js-libs if node wasnt selected
     check3.prop('disabled', false);
   }
-  if (check5.is(':checked')) {
+  if (check5.is(':checked')) {                   //If build-tools is selected add $100 to total
     $total += 100
   }
-  if (check6.is(':checked')) {
+  if (check6.is(':checked')) {                   //If npm is selected add $100 to total
     $total += 100
   }
-  if (check7.is(':checked')) {
+  if (check0.is(':checked')) {                   //If main conference is selected add $200 to total
     $total += 200
   }
 
-  $('.totalDiv').removeClass('is-hidden');
+  $('.totalDiv').removeClass('is-hidden');       // hides total amount 
 
-  $('.total-display').html('Total: $' + parseInt($total));
+  $('.total-display').html('Total: $' + parseInt($total)); // shows total amount of items selected
 };
 
-// listens when all are clicked 
-check1.on('click', checkStatus);
-check2.on('click', checkStatus);
-check4.on('click', checkStatus);
-check3.on('click', checkStatus);
-check5.on('click', checkStatus);
-check6.on('click', checkStatus);
-check7.on('click', checkStatus);
-
+// listens when each item is clicked 
+check1.add(check2).add(check3).add(check4).add(check5).add(check6).add(check0).on('click', checkStatus);
 
 $('.activities').append('<div class="totalDiv"><label name="total-amount" class="total-display">Total: </label></div>');
 $('.totalDiv').addClass('is-hidden');
 
+// hides all options before a payment selection
 $(".credit-card").hide();
 $("p:first").hide();
 $("p:last").hide()
-
-
-// /Variables needed in sections
-
-const ccNumber = document.getElementById("cc-num");
-const zip = document.getElementById("zip");
-const cvv = document.getElementById("cvv");
-const exDate = document.getElementById("exp-month");
-const exYear = document.getElementById("exp-year");
-const username = document.getElementById("name");
 
 //Display payment sections based on payment option selectedValue
 
@@ -118,7 +102,7 @@ $("select#payment option[value='credit card']").attr("selected", "selected"); //
 $(".credit-card").show() // shows the credit field due to it being defaulted
 
 $('#payment').on("change", function () {
-   if ($(this).val() == 'credit card') {                   // if the selected value is credit card show the credit card information
+  if ($(this).val() == 'credit card') {                   // if the selected value is credit card show the credit card information
     $(".credit-card").show();
   } else {
     $(".credit-card").hide();                             // if it was not selected hide it
@@ -143,14 +127,14 @@ function creditCardValidation() {
     $('#cc-num').prev().removeClass('errorText'); // if the condition is met removes red highlighted validation 
     $('#cc-num').removeClass('errorBox');
     return true;
-  } else {
+  } else {          // warns the user what should be inputted including a red highlighted box
     $('#cc-num').prev().addClass('errorText');
-    $('#cc-num').prev().text("Please enter a number that is between 13 and 16 digits long."); // warns the user what should be inputted 
+    $('#cc-num').prev().text("Please enter a number that is between 13 and 16 digits long.");
     $('#cc-num').addClass('errorBox');
   }
 }
 
-$('#cc-num').on('focusout', (e) => { // changes due to switiching between text input fields
+$('#cc-num').on('focusout', (e) => { // active when swtiching between text input fields
   creditCardValidation();
 })
 
@@ -159,178 +143,115 @@ function zipCodeValidation() {
 
   const cardRegex1 = /^\d{5}$/;     // only accepts a number 5 digits long 
   if (cardRegex1.test($('#zip').val())) {
-    $('#zip').prev().text("Zip Code:");
-    $('#zip').prev().removeClass('errorText');
+    $('#zip').prev().text("Zip Code:"); // adds the text ( Zip Code: ) back when the condition is met
+    $('#zip').prev().removeClass('errorText');// if the condition is met removes red highlighted validation 
     $('#zip').removeClass('errorBox');
     return true;
-  } else {
+  } else {                // warns the user what should be inputted including a red highlighted box
     $('#zip').prev().text("Please enter a valid zip code.")
     $('#zip').prev().addClass('errorText');
     $('#zip').addClass('errorBox');
   }
 }
 
-
-//Live zip code validation (calls on the function above)
-$('#zip').on('focusout', (e) => {
+$('#zip').on('focusout', (e) => {   // active when swtiching between text input fields
   zipCodeValidation();
 });
 
-//Validation Function for CVV #
-function finalCVVVal() {
-  //if credit card 5 digits long..
-  const cardRegex2 = /^\d{3}$/;
+
+function cvvValidation() {
+
+  const cardRegex2 = /^\d{3}$/;   // only accepts a number 3 digits long 
   if (cardRegex2.test($('#cvv').val())) {
-    $('#cvv').prev().text("CCV:");
-    $('#cvv').prev().removeClass('errorText');
+    $('#cvv').prev().text("CCV:");  // adds the text ( CCV: ) back when the condition is met
+    $('#cvv').prev().removeClass('errorText');// if the condition is met removes red highlighted validation 
     $('#cvv').removeClass('errorBox');
     return true;
-  } else {
+  } else {           // warns the user what should be inputted including a red highlighted box
     $('#cvv').prev().text("Please enter the 3-digit CVV found on the back of the card.")
     $('#cvv').prev().addClass('errorText');
     $('#cvv').addClass('errorBox');
   }
 };
 
-//Live ccv validation
-$('#cvv').on('focusout', (e) => {
-  finalCVVVal();
+
+$('#cvv').on('focusout', (e) => {   // active when swtiching between text input fields
+  cvvValidation();
 })
-function finalCVVVal2() {
-  //if credit card 5 digits long..
-  const cardRegex3 = /[A-Za-z]/
+function nameValidation() {
+
+  const cardRegex3 = /[A-Za-z]/   // only accepts upper case and lower care letters
   if (cardRegex3.test($('#name').val())) {
-    $('#name').prev().text("Name:");
-    $('#name').prev().removeClass('errorText');
+    $('#name').prev().text("Name:");   // adds the text ( Name: ) back when the condition is met
+    $('#name').prev().removeClass('errorText'); // if the condition is met removes red highlighted validation 
     $('#name').removeClass('errorBox');
     return true;
-  } else {
+  } else {                        // warns the user what should be inputted including a red highlighted box
     $('#name').prev().text("Please enter a name")
     $('#name').prev().addClass('errorText');
     $('#name').addClass('errorBox');
   }
 };
 
-
-$('#name').on('focusout', (e) => {
-  finalCVVVal2();
+$('#name').on('focusout', (e) => {    // active when swtiching between text input fields
+  nameValidation();
 })
-function finalCVVVal3() {
+function emailValidation() {
   //if credit card 5 digits long..
-  const cardRegex4 = /[A-Za-z]+@[a-zA-Z]+.[a-zA-z]+/
+  const cardRegex4 = /[A-Za-z]+@[a-zA-Z]+.[a-zA-z]+/    // only accepts example: example@gmail.com
   if (cardRegex4.test($('#mail').val())) {
-    $('#mail').prev().text("Email:");
-    $('#mail').prev().removeClass('errorText');
+    $('#mail').prev().text("Email:");             // adds the text ( Email: ) back when the condition is met
+    $('#mail').prev().removeClass('errorText');   // if the condition is met removes red highlighted validation 
     $('#mail').removeClass('errorBox');
     return true;
-  } else {
+  } else {                         // warns the user what should be inputted including a red highlighted box
     $('#mail').prev().text("Please enter a valid email")
     $('#mail').prev().addClass('errorText');
     $('#mail').addClass('errorBox');
   }
 };
 
-
-mail.addEventListener('focusout', (e) => {
-  finalCVVVal3();
+$('#mail').on('focusout', (e) => {    // active when swtiching between text input fields
+  emailValidation();
 })
 
-// $('form').submit(function () {
-//   // $("").keyup(function() {
-//     if ( $('input').val() == "") {
-//         alert('Text-field is empty.');
-           
-//   }  
-// })
-
-// $('input[type="checkbox"]').submit(function(){
-//   if($(this). prop("checked") == true){
-//   alert("Checkbox is checked." );
-//   }else if($(this). prop("checked") == false){
-//     alert("Checkbox is unchecked." );
-//   }})
 
 
-  
+$('form').on("submit", function (evt) {   // will not submit unless one activity is selected
 
-// $('form').submit(function () {
-
-//     // Get the Login Name value and trim it
-//     const nameField = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/
-//     const nameField1 = /[a-zA-Z]/
-
-//     // Check if empty of not
-//     if (nameField.test($('#mail').val()) == "") {
-//         alert('Text-field is empty.');;
-//     } else {
-      
-//     }
-//     if (nameField1.test($('#name').val())== "") {
-//       alert('Text-field is empty.');
-//       return false;
-// }});
-
-
-
-
-// $("#name").submit(function (e) {
-//   finalCVVVal2()
-//   return true;
-// });
-
-
-// //Email field
-// function errorEmail() {
-//   console.log(name);
-//   const emailVal = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   //If email doesn't have any text...
-//   if (email.value === "") {
-//     email.previousElementSibling.classList.add('errorText');
-//     email.previousElementSibling.innerText = "Oops! You forgot to enter your email.";
-//     email.classList.add('errorBox');
-//     //If email is correctly formatted...
-//   } else if (email.value.match(emailVal)) {
-//     email.previousElementSibling.textContent = "Email:";
-//     email.previousElementSibling.classList.remove('errorText');
-//     email.classList.remove('errorBox');
-//     return true;
-//     //If email is incorrectly formatted...
-//   } else {
-//     email.previousElementSibling.classList.add('errorText');
-//     email.previousElementSibling.innerText = "Please enter a valid email address.";
-//     email.classList.add('errorBox');
-//     return false;
-//   }
-// }
-
-
-$('form').on("submit", function (evt) {
-
-  if ($("input[type='checkbox']:checked").length == 0 ) {
-  
-     evt.preventDefault();
-     $("form submit").attr("disabled", true);
-
-     //disable a normal button
-     $("form submit").attr("disabled", true);
-
-     
-     $('checkbox').prev().text("Email:");
-     $('checkbox').prev().removeClass('errorText');
-     $('checkbox').removeClass('errorBox');
-     
-   } else {
-     $('checkbox').prev().text("Please enter a valid email")
-     $('checkbox').prev().addClass('errorText');
-     $('checkbox').addClass('errorBox');
-    //  $('#mail').prev().text("Please enter a valid email")
-    //  error.style.color = "crimson"
-    //  var errorContainer = document.getElementById("error-container"); // looks maybe something like this in your HTML: <div id="activities"><div id="error-container"></div></div>
-    //  errorContainer.innerHTML = "<h3>Choose at least one activity.</h3>"
-  return true 
+  if (!!(emailValidation() & cvvValidation() & zipCodeValidation() & creditCardValidation() & nameValidation())) {
+    // evt.preventDefault(); 
+    $('.activites legend').text('errorText')
+    return true;
   }
-  })
-  
-    // $('#mail').prev().addClass('errorText');
-    // $('#mail').addClass('errorBox');
+
+  // emailValidation() & cvvValidation() & zipCodeValidation() & creditCardValidation() 
+
+  evt.preventDefault();
+  // $("form submit").attr("disabled", true);
+
+
+  // $("form submit").attr("disabled", true);
+
+  // // $('checkbox').prev().text("Email:");
+  // // $('checkbox').prev().removeClass('errorText');
+  // // $('checkbox').removeClass('errorBox');
+  // // $('checkbox').prev().text("Please enter a valid email")
+  // // $('checkbox').prev().addClass('errorText');
+  // // $('checkbox').addClass('errorBox');
+  // $('fieldset legend').addClass('errorText')
+  // $(".activites").replace().parent().text('Choose at least one activity').addClass('errorText')
+
+
+  // $('.activities').addClass('errorBox', 'errorText')
+
+  //  error.style.color = "crimson"
+  //  var errorContainer = document.getElementById("error-container"); // looks maybe something like this in your HTML: <div id="activities"><div id="error-container"></div></div>
+  //  errorContainer.innerHTML = "<h3>Choose at least one activity.</h3>"
+  // return true
+
+}
+)
+
+
 
